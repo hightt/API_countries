9527cbeaac5a7da4d70bf8d8d2389cfe46668590
Konrad Duda <konrad.duda121@gmail.com>
2023-02-11 12:42:24 +0100

    index.blade.php - refactor to component usage

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,34 +1,16 @@
 @extends('app')
 
-@section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-4">
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <div class="dropdown">
-                        <a class="dropdown-toggle" href="#" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown link
-                        </a>
-
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
+@section('index')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2 p-0 pt-1">
+                <x-menu-list />
+            </div>
+            <div class="col-lg-10">
+                <div class="main-box mb-2 p-3">
+                    @yield('content')
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
